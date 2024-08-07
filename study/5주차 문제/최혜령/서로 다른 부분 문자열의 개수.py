c359dc6653ea780d5dae16f14f3bc6d2c465bb60
Ellie Choi <73865334+EllieChoi1998@users.noreply.github.com>
2024-08-07 11:18:18 +0900

    Create 서로 다른 부분 문자열의 개수.py

---
+++ study/5주차 문제/최혜령/서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,9 @@
+string = input().rstrip()
+
+substrings = set()
+
+for i in range(len(string)):
+    for j in range(i + 1, len(string) + 1):
+        substrings.add(string[i:j])
+
+print(len(substrings))
