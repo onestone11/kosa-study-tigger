8af1dea20e27381a17746f39d9f825088c8436f0
kjs0314 <168392209+kjs0314@users.noreply.github.com>
2024-10-30 09:52:54 +0900

    Update 큰 수 만들기.java

--- 아기호랑이반/1주차 문제/김진석/큰 수 만들기.java
+++ 아기호랑이반/1주차 문제/김진석/큰 수 만들기.java
@@ -1,6 +1,20 @@
 class Solution {
-    public String solution(String number, int k) {
-        String answer = "";
-        return answer;
+	public String solution(String number, int k) {
+        
+		int index = 0;
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < number.length() - k; i++) {
+			char max = 0;
+			for(int j = index; j <= i + k; j++) {
+				if(max < number.charAt(j)) {
+					max = number.charAt(j);
+					index = j + 1;
+				}
+			}
+			sb.append(max);
+		}
+        
+        return sb.toString();
     }
 }
