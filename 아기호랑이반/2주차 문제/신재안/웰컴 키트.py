dcda3be558eb47c606ccf04ba46b7a0d6eec9eb5
havegrit <79460763+havegrit@users.noreply.github.com>
2024-11-06 23:12:03 +0900

    Create 웰컴 키트.py

---
+++ 아기호랑이반/2주차 문제/신재안/웰컴 키트.py
@@ -0,0 +1,16 @@
+n = int(input())
+sizes = list(map(int, input().split()))[:6]
+t, p = map(int, input().split())
+t_order = 0
+
+p1 = n // p
+p2 = n % p
+
+for size in sizes:
+    t_order += size // t
+    size = size % t
+    if size > 0:
+        t_order += 1
+        
+print(t_order)
+print(f"{p1} {p2}")
