fdec20f89a531787160ba8bc1a804213751575f7
이다영 <106363495+yooung513@users.noreply.github.com>
2025-01-15 19:36:05 +0900

    Update 등굣길.java

--- 엄마호랑이반/9주차 문제/이다영/등굣길.java
+++ 엄마호랑이반/9주차 문제/이다영/등굣길.java
@@ -1,24 +1,23 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] dp = new int[n + 1][m + 1];
-		dp[1][1] = 1;
-		
-		
-		for (int[] puddle : puddles) {
-			dp[puddle[1]][puddle[0]] = -1;
-		}
-		
-		for (int x = 1; x < n + 1; x++) {
-			for (int y = 1; y < m + 1; y++) {
+	dp[1][1] = 1;
+	
+	for (int[] puddle : puddles) {
+		dp[puddle[1]][puddle[0]] = -1;
+	}
+	
+	for (int x = 1; x < n + 1; x++) {
+		for (int y = 1; y < m + 1; y++) {
+			
+			if(dp[x][y] != -1) {
+				dp[x][y] += (dp[x-1][y] + dp[x][y-1]) % 1_000_000_007;
 				
-				if(dp[x][y] != -1) {
-					dp[x][y] += (dp[x-1][y] + dp[x][y-1]) % 1_000_000_007;
-					
-				} else {
-					dp[x][y] = 0;
-				}
+			} else {
+				dp[x][y] = 0;
 			}
 		}
-		return dp[n][m] % 1_000_000_007;
+	}
+	return dp[n][m] % 1_000_000_007;
     }
 }
