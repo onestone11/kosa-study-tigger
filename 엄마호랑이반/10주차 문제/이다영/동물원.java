ffe5d8f809beb7e903ef7eb2209bf935abe6a8b5
이다영 <106363495+yooung513@users.noreply.github.com>
2025-01-20 15:02:12 +0900

    Create 동물원.java

---
+++ 엄마호랑이반/10주차 문제/이다영/동물원.java
@@ -0,0 +1,22 @@
+package temp_dev;
+
+import java.util.*;
+import java.io.*;
+
+public class Boj {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] dp = new int[n+1];
+		dp[0] = 1;
+		dp[1] = 3;
+		
+		for(int i = 2; i < n + 1; i++) {
+			dp[i] = ((dp[i-1] * 2) + dp[i-2]) % 9901;
+		}
+		
+		System.out.print(dp[n]);
+	}
+	
+}
