5212a5867998c08fd892305a97859f1b2172f21a
onestone11 <onestone1380@gmail.com>
2025-03-26 20:53:25 +0900

    Update 올바른괄호의개수.java

--- 엄마호랑이반/17주차 문제/장원석/올바른괄호의개수.java
+++ 엄마호랑이반/17주차 문제/장원석/올바른괄호의개수.java
@@ -1,16 +1,31 @@
-class Solution {
-    public int solution(int n) {
-        int[] dp = new int[n+1];
-        
-        dp[0] = 1;
-        dp[1] = 1;
-        
-        for(int i = 2; i<=n; i++){ 
-            for(int j=1; j<=i; j++){
-                dp[i] += dp[i-j] * dp[j-1]; 
-            }
-        }
-  
-        return dp[n];
-    }
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class 올바른괄호의개수 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(2));
+		System.out.println(solution(3));
+		System.out.println(solution(4));
+		System.out.println(solution(5));
+		System.out.println(solution(6));
+	}
+
+	private static int solution(int n) {
+
+		int[] dp = new int[n + 1];
+
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				dp[i] += dp[i - j] * dp[j - 1];
+			}
+		}
+
+		return dp[n];
+	}
 }
