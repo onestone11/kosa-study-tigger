268061ba2525f3aa1609b985871969f6bfbb83ff
kjs0314 <168392209+kjs0314@users.noreply.github.com>
2025-06-17 18:33:55 +0900

    Create 단속카메라.java

---
+++ 엄마호랑이반/26주차 문제/김진석/단속카메라.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int out = routes[0][1];
+        int answer = 1;
+        
+        for(int i = 1; i < routes.length; i++) {
+            if(routes[i][0] > out) {
+                out  = routes[i][1];
+                answer++;
+            } 
+        }
+        return answer;
+    }
+}
