61b6e38d07e26697ee526f8fc6fe4773bd37e931
isolated-hwan <150107824+isolated-hwan@users.noreply.github.com>
2025-06-24 17:40:13 +0900

    Create 집합의 표현.py

---
+++ 엄마호랑이반/27주차 문제/박정환/집합의 표현.py
@@ -0,0 +1,32 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+n, m  = map(int , input().split())
+
+# 각 노드의 부모를 자기 자신으로 초기화
+p = [i for i in range(n+1)]
+
+# 루트 노드를 찾는 Find 연산
+def find(a):
+    if p[a] != a:
+        p[a] = find(p[a])
+    return p[a]
+
+# 두 노드를 같은 집합으로 합치는 Union 연산
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b: # 더 작은 값이 부모 노드가 될 수 있도록
+        p[b] = a
+    else:
+        p[a] = b
+
+for _ in range(m):
+    s, a, b = map(int, input().split())
+    if s == 0:
+        union(a, b)
+    else:
+        if find(a) != find(b):
+            print("NO")
+        else:
+            print("YES")
